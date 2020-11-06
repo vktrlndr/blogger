18f38c54517efa9eb054809ef7f41ba634c10cea
Viktor Ålander <viktor.alander@humanit.se>
2020-11-06 13:33:24 +0100

    Initial commit

---
+++ src/main/java/se/newton/blogger/BloggerApplication.java
@@ -0,0 +1,13 @@
+package se.newton.blogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerApplication.class, args);
+	}
+
+}
